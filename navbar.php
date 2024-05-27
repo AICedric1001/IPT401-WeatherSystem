e3d2d33606648d7672b2075f3a655c8ee981cbe0
AlCedric1001 <alcedricabarientos14@gmail.com>
2024-05-27 11:08:35 +0800

    navbar?

--- navbar.php
+++ navbar.php
@@ -5,7 +5,10 @@ if (session_status() == PHP_SESSION_NONE) {
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Ginhawa</a>
+    <a class="navbar-brand" href="#">
+        <img src="bg1.jpg" width="30" height="30" class="d-inline-block align-top" alt="Logo">
+        Ginhawa
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
